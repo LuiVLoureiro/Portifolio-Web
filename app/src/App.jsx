2e13215cedb2e4ab10a03bffbe1ab5af5a06d98e
LuiLoureiro <luivloureiro@gmail.com>
2024-09-19 06:18:36 -0300

    ftr: Adicionar seção de Educação

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import WorkExperience from "./components/WorkExperience";
+import EducationSection from "./components/EducationSection";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Projects />
         <Skills />
         <WorkExperience />
+        <EducationSection />
       </div>
     </div>
   )
