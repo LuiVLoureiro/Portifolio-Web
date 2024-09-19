02a865d28d9813f0341cec6c8495c7b8f70d2968
LuiLoureiro <luivloureiro@gmail.com>
2024-09-19 06:09:11 -0300

    ftr: Adicionar sessão de Skills

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects"
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Hero />
         <Navbar />
         <Projects />
+        <Skills />
       </div>
     </div>
   )
